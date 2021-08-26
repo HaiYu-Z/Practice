966d276bf4a7f5b074eb12a99ccf29ac549c7403
HaiYu0 <yu-0712@qq.com>
2021-08-26 22:09:53 +0800

    test

---
+++ Java/person/ExtendsTest.java
@@ -0,0 +1,11 @@
+package com.test.person;
+
+public class ExtendsTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.age = 18;
+
+        Student s1 = new Student();
+        s1.eat();
+    }
+}
---
+++ Java/person/Student.java
@@ -0,0 +1,20 @@
+package com.test.person;
+
+public class Student extends Person {
+
+    String major;
+
+    public Student() {
+
+    }
+    public Student(String name, int age, String major) {
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+
+    public void study() {
+        System.out.println("学习");
+    }
+}
