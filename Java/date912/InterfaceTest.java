f3f684a64368b3033a2bb9545a3ab68191de0cc1
HaiYu <yu-0712@qq.com>
2021-09-12 22:46:08 +0800

    date912

--- Java/date912/InterfaceTest.java
+++ Java/date912/InterfaceTest.java
@@ -1,4 +1,35 @@
 package practice.date912;
 
 public class InterfaceTest {
+    public static void main(String[] args) {
+        System.out.println(Flyable.MAX_SPEED);
+        System.out.println(Flyable.MIN_SPEED);
+
+        Plane p = new Plane();
+        p.fly();
+        p.stop();
+    }
+}
+
+interface Flyable {
+    //全局常量
+    public static final int MAX_SPEED = 7900;//第一宇宙速度
+    int MIN_SPEED = 1;//省略了 public static final
+
+    //抽象方法
+    public abstract void fly();
+    void stop();
+}
+
+class Plane implements Flyable {
+
+    @Override
+    public void fly() {
+        System.out.println("通过引擎起飞");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("驾驶员减速停止");
+    }
 }
